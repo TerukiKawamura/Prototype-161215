cb297db310d7a28402e8d969ff9ae99da0ee2e12
TerukiKawamura <te-kawamura@mic.jp>
2016-12-20 08:46:31 +0900

    レスポンス内容修正

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -18,7 +18,6 @@ package com.example.bot.spring.echo;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
@@ -39,6 +38,7 @@ public class EchoApplication {
     }
 
     private String[] week_name = {"日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"};
+    private String[] weather_name = {"晴れ", "晴れのち雨", "雨", "くもり", "くもりのち晴れ", "雪", "雨のち晴れ"};
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -55,26 +55,33 @@ public class EchoApplication {
 			//int year = calendar.get(Calendar.YEAR);
 			//int month = calendar.get(Calendar.MONTH) + 1;
 			//int day = calendar.get(Calendar.DATE);
-			//int hour = calendar.get(Calendar.HOUR_OF_DAY);
+			int hour = calendar.get(Calendar.HOUR_OF_DAY);
 			//int minute = calendar.get(Calendar.MINUTE);
 			//int second = calendar.get(Calendar.SECOND);
 		    int week = calendar.get(Calendar.DAY_OF_WEEK) - 1;
 
-        	if (message.contains("今日"))
-        	{
-        		if (message.contains("何曜"))
-        		{
-        			responseMessage = week_name[week] + "です";
+        	if (message.contains("今日")) {
+        		if (message.contains("何曜")) {
+        			responseMessage = String.format("%sです", week_name[week]);
+        		} else if (message.contains("何日")) {
+        			SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        			responseMessage = String.format("%sです", sdf.format(calendar.getTime()));
         		}
-        	} else if (message.contains("次")) {
-        		if (message.contains("予約")) {
-        			calendar.add(Calendar.DATE, 10);
-        			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        			responseMessage = sdf.format(calendar.getTime()) + "の15時です";
+        	} else if (message.contains("明日")) {
+
+    		} else if (message.contains("次")) {
+	    		if (message.contains("予約")) {
+	    			calendar.add(Calendar.DATE, 10);
+	    			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+	    			responseMessage = String.format("%sの%d時です", sdf.format(calendar.getTime()), hour - 2 % 24);
+	    		}
+    		} else {
+        		if (message.contains("天気")) {
+        			responseMessage = String.format("%sです", weather_name[week]);
+        		} else if (message.contains("ここはどこ")) {
+        			responseMessage = "地球です";
         		}
-        	} else if (message.contains("天気")) {
-    			responseMessage = "晴れたらいいね";
-        	}
+    		}
         }
         
         return new TextMessage(responseMessage);
